3934cf2ad618cfe32919ff898f9534eee293f2b9
x233993 <Spencer.Avinger@wnco.com>
2017-10-02 18:36:31 -0500

    initial commit

---
+++ test.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import Enzyme, { mount } from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+
+Enzyme.configure({ adapter: new Adapter() });
+
+class MyComponent extends React.Component {
+  
+  componentDidMount() {
+    this.ref.classList.add('test');
+  }
+  
+  render() {
+    return (
+      <div>
+        <div ref={ el => { this.ref = el; }}></div>
+      </div>
+    );
+  }
+}
+
+describe('MyComponent', () => {
+  
+  it('should render', () => {
+    
+    let wrapper = mount(<MyComponent />);
+    
+    // element with "test" class is visible in markup
+    console.log(wrapper.html()); 
+    
+    // returns 0
+    expect(wrapper.find('.test').length).toBe(1);
+  });
+});
